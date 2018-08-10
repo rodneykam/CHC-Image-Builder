f0648f37649753f819eb815f21d8aef0d01736ec
RodneyKam <rodney.kam@relayhealth.com>
2018-08-10 13:43:07 -0700

    Add project files.

---
+++ CHC-Image-Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHC_Image_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
